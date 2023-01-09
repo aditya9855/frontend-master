0af0f2b0792aec088af266190d96507eda18a8b0
aditya9855 <aditya9855@gmail.com>
2023-01-09 10:16:12 +0530

    function

---
+++ ydk.js
@@ -0,0 +1,12 @@
+var favoriteBook = [];
+
+addFavoriteBook("you don't know js");
+addFavoriteBook("mind casting ");
+
+
+function addFavoriteBook(bookName) {
+    console.log(bookName);
+
+}
+
+addFavoriteBook();
