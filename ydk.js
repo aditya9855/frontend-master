29a04e721058de50ec56185b6826ebee0dc76283
aditya9855 <aditya9855@gmail.com>
2023-01-09 10:27:49 +0530

    skdn

--- ydk.js
+++ ydk.js
@@ -3,10 +3,31 @@ var favoriteBook = [];
 addFavoriteBook("you don't know js");
 addFavoriteBook("mind casting ");
 
-
 function addFavoriteBook(bookName) {
-    console.log(bookName);
-
+  console.log(bookName);
+  favoriteBook.push(bookName);
 }
 
-addFavoriteBook();
+addFavoriteBook("you find this very easy");
+
+console.log(favoriteBook);
+
+
+var favoriteBook = [];
+
+addFavoriteBook("you don't know js");
+addFavoriteBook("mind casting ");
+
+function addFavoriteBook(bookName) {
+  console.log(bookName);
+  favoriteBook.push(bookName);
+}
+
+addFavoriteBook("you find this very easy");
+
+console.log(favoriteBook);
+
+function printFavoriteBook() {
+  console.log(`favorite Book : ${favoriteBook.length}`);
+}
+printFavoriteBook();
